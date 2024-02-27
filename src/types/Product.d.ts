f15a592d042c81721256ebb318c510cfe74b36b0
TCxDouglas <douglasalexander683@gmail.com>
2024-02-26 20:30:00 -0600

    chore: Removed Types

---
+++ src/types/Product.d.ts
@@ -0,0 +1,9 @@
+export interface Product {
+    id: number
+    name: string
+    description: string
+    price: number
+    country: string
+    createdAt: string
+    updatedAt: string
+}
--- src/types/TypePhone.d.ts
+++
@@ -1,6 +0,0 @@
-export interface TypePhone {
-  id: number;
-  type: string;
-  createdAt: string;
-  updatedAt: string;
-}
